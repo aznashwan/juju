f0e8424913e169058aba0b3b99f65f5e6f2466c2
aznashwan <aznashwan@yahoo.com>
2014-10-01 12:27:07 +0300

    Added test for juju-reboot command.

--- worker/uniter/jujuc/reboot_test.go
+++ worker/uniter/jujuc/reboot_test.go
@@ -45,92 +45,53 @@ func (s *JujuRebootSuite) TestSetFlags(c *gc.C) {
 	c.Assert(flag, gc.NotNil)
 }
 
-// func (s *JujuRebootSuite) TestRunRebootNow(c *gc.C) {
-// 	ctx := Context{rebootPrio: jujuc.RebootSkip}
-// 	rebootCmd := jujuc.NewJujuRebootCommand(&ctx)
-// 	jujuRebootCmd, ok := rebootCmd.(*jujuc.JujuRebootCommand)
-// 	c.Assert(ok, jc.IsTrue)
-// 	jujuRebootCmd.Now = true
-
-// 	err := jujuRebootCmd.Run(nil)
-// 	c.Assert(err, gc.IsNil)
-
-// 	c.Assert(ctx.rebootPrio, gc.Equals, jujuc.RebootNow)
-// }
-
-// func (s *JujuRebootSuite) TestRunRebootAfterHook(c *gc.C) {
-// 	ctx := Context{rebootPrio: jujuc.RebootSkip}
-// 	rebootCmd := jujuc.NewJujuRebootCommand(&ctx)
-// 	jujuRebootCmd, ok := rebootCmd.(*jujuc.JujuRebootCommand)
-// 	c.Assert(ok, jc.IsTrue)
-// 	jujuRebootCmd.Now = false
-
-// 	err := jujuRebootCmd.Run(nil)
-// 	c.Assert(err, gc.IsNil)
-
-// 	c.Assert(ctx.priority, gc.Equals, jujuc.RebootAfterHook)
-// }
-
-// func (s *JujuRebootSuite) TestJujuRebootCommandSuccessHandling(c *gc.C) {
-// 	hctx := Context{giveError: false}
-
-// 	com, err := jujuc.NewCommand(hctx, cmdString("juju-reboot"))
-// 	c.Assert(err, gc.IsNil)
-// 	ctx := testing.Context(c)
-// 	code := cmd.Main(com, ctx, "")
-// 	c.Assert(code, gc.Equals, 0)
-// }
-
-// func (s *JujuRebootSuite) TestJujuRebootCommandFailureHandling(c *gc.C) {
-// 	hctx := Context{giveError: true}
-
-// 	com, err := jujuc.NewCommand(hctx, cmdString("juju-reboot"))
-// 	c.Assert(err, gc.IsNil)
-// 	ctx := testing.Context(c)
-// 	code := cmd.Main(com, ctx, "")
-// 	c.Assert(code, gc.Equals, 1)
-// }
-
 func (s *JujuRebootSuite) TestJujuRebootCommand(c *gc.C) {
 	var jujuRebootTests = []struct {
-		summary string
-		hctx    *Context
-		args    []string
-		code    string
+		summary 	string
+		hctx    	*Context
+		args    	[]string
+		code    	int
+		prio		jujuc.RebootPriority
 	}{{
-		summary: "test reboot priority defaulting to RebootAfterHook",
-		hctx:    &Context{giveError: false, rebootPrio: jujuc.RebootSkip},
-		args:    "",
-		code:    0,
+		summary:	"test reboot priority defaulting to RebootAfterHook",
+		hctx:		&Context{giveError: false, rebootPrio: jujuc.RebootSkip},
+		args:		[]string{},
+		code:		0,
+		prio:		jujuc.RebootAfterHook,
 	}, {
 		summary: "test reboot priority being set to RebootNow",
-		hctx:    &Context{giveError: false, rebootPrio: jujuc.RebootSkip},
-		args:    "now",
-		code:    0,
+		hctx:    	&Context{giveError: false, rebootPrio: jujuc.RebootSkip},
+		args:    	[]string{"now"},
+		code:    	0,
+		prio:		jujuc.RebootNow,
 	}, {
 		summary: "test a failed running of juju-reboot",
-		hctx:    &Context{giveError: true, rebootPrio: jujuc.RebootSkip},
-		args:    "",
-		code:    1,
+		hctx:    	&Context{giveError: true, rebootPrio: jujuc.RebootSkip},
+		args:    	[]string{},
+		code:    	1,
+		prio:		jujuc.RebootAfterHook,
 	}, {
 		summary: "test a failed running with parameter provided",
-		hctx:    &Context{giveError: true, rebootPrio: jujuc.RebootSkip},
-		args:    "now",
-		code:    1,
+		hctx:    	&Context{giveError: true, rebootPrio: jujuc.RebootSkip},
+		args:    	[]string{"now"},
+		code:    	1,
+		prio:		jujuc.RebootNow,
 	}, {
 		summary: "test invalid args provided",
-		hctx:    &Context{giveError: false, rebootPrio: jujuc.RebootSkip},
-		args:    "way too many args",
-		code:    0,
+		hctx:    	&Context{giveError: false, rebootPrio: jujuc.RebootSkip},
+		args:    	[]string{"way", " too", " many", " args"},
+		code:    	0,
+		prio:		jujuc.RebootSkip,
 	}}
 
 	for i, t := range jujuRebootTests {
 		c.Logf("Test %d: %s", i, t.summary)
 
-		com, err := jujuc.NewCommand(t.hctx, cmdInfo("juju-reboot"))
+		com, err := jujuc.NewCommand(t.hctx, cmdString("juju-reboot"))
 		c.Assert(err, gc.IsNil)
 		ctx := testing.Context(c)
-		code := cmd.Main(com, ctx, t.args)
-		c.Assert(code, gc.Equals, t.code)
+		code := cmd.Main(com, ctx, []string{t.args})
+		c.Check(code, gc.Equals, t.code)
+		c.Check(t.hctx.rebootPrio, gc.Equals, t.prio)
 	}
 }
